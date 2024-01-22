05f015fa63486672e0b49372b0b08c48ae77a5cc
Miguel Salazar <yobebe@hotmail.com.ar>
2024-01-22 01:28:44 -0300

    aprendiendo python en mi dia libre

---
+++ introduccionPython/primera_aplicacion_python.py
@@ -0,0 +1,19 @@
+# nombre del modulo.
+""" primera_aplicacion_python.py """
+
+# codigo de prueba. 
+print("Hola mundo!!" * 3)
+print("Yo soy el leon!!")
+print("Viva front-end carajo!!")
+
+# calculadora basica con python 
+numero1 = 30 
+numero2 = 45
+multiplicar = numero1 * numero2
+print("el resultado es", multiplicar)
+
+"""
+hasta aca llegamos por hoy, 
+mi idea es seguir aprendiendo python,
+pero por unos dias vamos a meterle pata a backend. 
+"""
